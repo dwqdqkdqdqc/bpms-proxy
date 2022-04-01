7ca868c134671e748264742e15eba7599cc8a102
irubtsov <irubtsov@sitronics.com>
2022-04-01 17:27:27 +0300

    ProcessDefinitionControllerTest | ProcessDefinitionConstants

--- src/test/java/ru/sitronics/tn/camundaproxyrestapi/process_definition/ProcessDefinitionConstants.java
+++ src/test/java/ru/sitronics/tn/camundaproxyrestapi/process_definition/ProcessDefinitionConstants.java
@@ -4,31 +4,35 @@ public final class ProcessDefinitionConstants {
 
     public static final String KEY = "id";
     public static final String START_PROCESS_URI = "/process-definition/key/{key}/start";
-    public static final String REQUEST_JSON = """
-                "variables": {
-               "aVariable": {
-                 "value": "aStringValue",
-                 "type": "String"
-               },
-               "anotherVariable": {
-                 "value": true,
-                 "type": "Boolean"
-               }
-             },
-             "businessKey": "myBusinessKey"
+    public static final String REQUEST_JSON = """ 
+            {
+              "variables": {
+                "aVariable": {
+                  "value": "aStringValue",
+                  "type": "String"
+                },
+                "anotherVariable": {
+                  "value": true,
+                  "type": "Boolean"
+                }
+              },
+              "businessKey": "myBusinessKey"
+            }
             """;
     public static final String RESPONSE_JSON = """
-               links": [
-             {
-               "method": "GET",
-               "href": "http://localhost:8080/rest-test/process-instance/anId",
-               "rel": "self"
-             }
-            ,
-            id": "anId",
-            definitionId": "aProcessDefinitionId",
-            businessKey": "myBusinessKey",
-            ended": false,
-            suspended": false
+            {
+              "links": [
+                {
+                  "method": "GET",
+                  "href": "http://localhost:8080/rest-test/process-instance/anId",
+                  "rel": "self"
+                }
+              ],
+              "id": "anId",
+              "definitionId": "aProcessDefinitionId",
+              "businessKey": "myBusinessKey",
+              "ended": false,
+              "suspended": false
+            }
             """;
 }
